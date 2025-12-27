199b1a3c17bbc72dba46de69fa3ecfb3acc874f6
Gabriel Medina <rha7.com@gmail.com>
2025-12-26 22:01:36 -0600

    Initial working app.

---
+++ app/main/page.tsx
@@ -0,0 +1,34 @@
+const MainPage = () => {
+  return (
+    <div className={"container mx-auto my-8"}>
+      <article
+        className={
+          "prose mx-auto bg-cyan-50 p-4 border border-cyan-500 rounded-lg"
+        }
+      >
+        <h1>Main Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet.
+        </p>
+        <p>
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
+          vero eros et accumsan et iusto odio dignissim qui blandit praesent
+          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+        </p>
+        <p>
+          Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+          suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem
+          vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+          consequat, vel illum dolore eu feugiat nulla facilisis.
+        </p>
+      </article>
+    </div>
+  );
+};
+
+export default MainPage;
