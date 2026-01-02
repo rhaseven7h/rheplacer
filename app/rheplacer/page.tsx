58e4f2e7e1bc75220469004741e961d37dff5aa7
Gabriel Medina <rha7.com@gmail.com>
2026-01-02 04:50:33 -0600

    Added a test admin layout using grid. Added a base atomic design folder structure.

---
+++ app/rheplacer/page.tsx
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Gabriel Medina (Rhaseven7h).
+ * All rights reserved.
+ *
+ * This source code is proprietary and confidential.
+ * Unauthorized copying of this file, via any medium, is strictly prohibited.
+ *
+ */
+
+import "./rheplacer.scss";
+
+const Rheplacer = () => {
+  return (
+    <div className={"rh-grid"}>
+      <div className={"rh-header"}>Nav Bar</div>
+      <div className={"rh-sidebar"}>Side Bar</div>
+      <div className={"rh-content"}>Main Content</div>
+      <div className={"rh-footer"}>Footer Bar</div>
+    </div>
+  );
+};
+
+export default Rheplacer;
